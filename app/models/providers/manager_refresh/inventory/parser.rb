989d3e585a7dbfc96a255ddacd258e144e63eb52
Adam Grare <agrare@redhat.com>
2018-04-11 10:05:33 -0400

    Remove graph refresh

--- app/models/providers/manager_refresh/inventory/parser.rb
+++
@@ -1,8 +0,0 @@
-class Providers::ManagerRefresh::Inventory::Parser
-  attr_accessor :collector
-  attr_accessor :persister
-
-  def parse
-    raise NotImplementedError, _("must be implemented in a subclass")
-  end
-end
